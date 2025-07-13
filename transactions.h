1cf911747525a9e7db9b68c8f0c74e0911d475eb
VARSHA TM <yazhini.0620@gmail.com>
2025-07-13 11:49:15 +0530

    Update transactions.h

--- transactions.h
+++ transactions.h
@@ -2,7 +2,7 @@
 #define TRANSACTIONS_H
 
 // Renamed function to avoid conflict with struct 'transaction'
-void do_transaction(void);
+void transaction(void);
 void update_balance(void);
 float give_balance(void);
 void add_to_file_transaction(void);
